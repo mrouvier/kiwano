7d999a7e22758af374338e36642c8a66241ec531
Mickael Rouvier <rouvier@u-mc016.univ-avignon.fr>
2023-09-08 09:49:34 +0200

    add cepstral mean normalization

---
+++ kiwano/features/base.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class CMVN:
+    """
+    Apply cepstral mean and (optionnaly) variance normalization
+
+    arr = [ [1, 2, 3], [3, 4, 5], [5, 6, 7] ]
+    CMVN(arr)
+
+    """
+    def __new__(self, arr: np.ndarray):
+        return arr - np.mean(arr, axis=0)
