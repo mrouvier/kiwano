6e5687c4082f88ff6115aa491a33aa74a5160545
mrouvier <mickael.rouvier@gmail.com>
2023-09-27 09:34:01 +0200

    modify script run.sh

--- recipes/resnet/run.sh
+++ recipes/resnet/run.sh
@@ -11,9 +11,10 @@ python3 local/download_musan.py db/musan/
 python3 local/prepare_musan.py db/musan/ data/musan/
 
 #Prepare RIRS NOISES
-python3 local/download_rirs_noises.py db/rirs_noises/
-python3 local/prepare_rirs_noises.py db/musan/ data/rirs_noises
+#python3 local/download_rirs_noises.py db/rirs_noises/
+#python3 local/prepare_rirs_noises.py db/musan/ data/rirs_noises
 
 
 #Train resnet
 python3 utils/train_resnet.py data/voxceleb1/
+python3 utils/extract_resnet.py exp/model3500.mat > exp/xvector.txt
