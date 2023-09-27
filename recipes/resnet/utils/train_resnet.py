308bfa198cb7f2d6cbeb35136c79e3208a4eb94a
mrouvier <mickael.rouvier@gmail.com>
2023-09-27 09:32:07 +0200

    add autocast and scaler

--- recipes/resnet/utils/train_resnet.py
+++ recipes/resnet/utils/train_resnet.py
@@ -49,6 +49,7 @@ class SpeakerTrainingSegmentSet(Dataset, SegmentSet):
 
 
 if __name__ == '__main__':
+    device = torch.device("cuda")
 
     musan = SegmentSet()
     musan.from_dict(Path("data/musan/"))
@@ -70,44 +71,56 @@ if __name__ == '__main__':
                                     feature_extractor=Fbank(),
                                     feature_transforms=Linear( [
                                         CMVN(),
-                                        Crop(300),
+                                        Crop(200),
                                     ] ),
                                 )
 
 
     training_data.from_dict(Path("data/voxceleb1/"))
 
-    train_dataloader = DataLoader(training_data, batch_size=128, drop_last=True, shuffle=True, num_workers=10)
+    train_dataloader = DataLoader(training_data, batch_size=128, drop_last=True, shuffle=True, num_workers=20)
     iterator = iter(train_dataloader)
 
     resnet_model = ResNet()
+    resnet_model.to(device)
 
-    optimizer = torch.optim.SGD(resnet_model.parameters(), lr=0.2, momentum=0.9)
+    optimizer = torch.optim.SGD(resnet_model.parameters(), lr=0.1, momentum=0.9, weight_decay=0.0001)
     criterion = nn.CrossEntropyLoss()
 
+    scaler = torch.cuda.amp.GradScaler(enabled=True)
+
+
     num_iterations = 150000
-    for iterations in range(0, num_iterations):
-        feats, iden = next(iterator)
-        feats = feats.unsqueeze(1)
+    iterations = 0
+    for epochs in range(0, 150):
+        for feats, iden in train_dataloader:
 
-        preds = resnet_model(feats, iden)
+            feats = feats.unsqueeze(1)
 
-        loss = criterion(preds, iden)
+            feats = feats.to(device)
+            iden = iden.to(device)
 
-        optimizer.zero_grad()
+            optimizer.zero_grad()
 
-        loss.backward()
-
-        optimizer.step()
-
-        msg = "{}: [{}/{}] \t C-Loss:{:.4f}".format(time.ctime(), iterations, num_iterations, loss.item())
-        print(msg)
+            with torch.cuda.amp.autocast(enabled=True):
+                preds = resnet_model(feats, iden)
+                loss = criterion(preds, iden)
 
 
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
 
+            #loss.backward()
+            #optimizer.step()
 
+            if iterations%100 == 0:
+                msg = "{}: [{}/{}] \t C-Loss:{:.4f}".format(time.ctime(), iterations, num_iterations, loss.item())
+                print(msg)
 
+            if iterations%500 == 0:
+                torch.save(resnet_model, "exp/model"+str(iterations)+".mat")
 
-
+            iterations += 1
 
      
