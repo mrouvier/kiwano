487bfbba29960cf49f43f42569afa1c311986ee6
mmlamah <lamahmichelmarie@gmail.com>
2023-10-24 12:51:00 +0200

    test sur serveur

---
+++ recipes/resnet/kiwano_slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=kiwano
+#SBATCH --partition=gpu
+#SBATCH --time=0-00:00
+#SBATCH --output=kiwano_output.log
+#SBATCH --error=kiwano_error.log
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=lamahmichelmarie@gmail.com
+
+source /etc/profile.d/conda.sh
+conda activate kiwano
+
+python3 utils/train_wav2vec2.py
+
+conda deactivate
