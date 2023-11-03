32bd924828174c6a8341bac3e6a314cb07aacdfc
miguel <lamahmichelmarie@gmail.com>
2023-11-03 23:36:56 +0100

    03 Nov 2023 23:36

--- kiwano/model/ecapa_tdnn.py
+++ kiwano/model/ecapa_tdnn.py
@@ -81,6 +81,19 @@ class Bottle2neck(nn.Module):
         out += residual
         return out
 
+class PreEmphasis(torch.nn.Module):
+
+    def __init__(self, coef: float = 0.97):
+        super().__init__()
+        self.coef = coef
+        self.register_buffer(
+            'flipped_filter', torch.FloatTensor([-self.coef, 1.]).unsqueeze(0).unsqueeze(0)
+        )
+
+    def forward(self, input: torch.tensor) -> torch.tensor:
+        input = input.unsqueeze(1)
+        input = F.pad(input, (1, 0), 'reflect')
+        return F.conv1d(input, self.flipped_filter).squeeze(1)
 
 class FbankAug(nn.Module):
 
