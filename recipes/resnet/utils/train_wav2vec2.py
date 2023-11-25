8689de74d9ff9a1fbb57e82d7516187d0ea0ef06
miguel <lamahmichelmarie@gmail.com>
2023-11-25 01:02:54 +0100

    25 nov 2023 01:02

--- recipes/resnet/utils/train_wav2vec2.py
+++ recipes/resnet/utils/train_wav2vec2.py
@@ -59,7 +59,8 @@ if __name__ == '__main__':
     )
     training_data.from_dict(Path("data/voxceleb1/"))
 
-    train_dataloader = DataLoader(training_data, batch_size=400, drop_last=True, shuffle=False, num_workers=50)
+    # batch_size=400
+    train_dataloader = DataLoader(training_data, batch_size=10, drop_last=True, shuffle=False, num_workers=50)
     print("END Loading data")
     sys.stdout.flush()
 
