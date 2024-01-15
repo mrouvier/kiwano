8d59c79cf0bf9d60b58ea23550ed78d4df957680
mmlamah <lamahmichelmarie@gmail.com>
2024-01-15 12:36:41 +0100

    15 janv 2024 12:36

--- recipes/resnet/eval_ecapa.sh
+++ recipes/resnet/eval_ecapa.sh
@@ -3,8 +3,8 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=128GB
-#SBATCH --cpus-per-task=20
+# #SBATCH --mem=128GB
+#SBATCH --cpus-per-task=5
 #SBATCH --output=eval_output.log
 #SBATCH --error=eval_error.log
 
@@ -12,7 +12,7 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-# python3  trainECAPAModel.py --eval --initial_model exps/exp4/model/model_0001.model  --feat_type wav2vec2 --feat_dim 768
-# python3 trainECAPAModel.py --eval --initial_model exps/pretrain.model --feat_type fbank --feat_dim 80
+# python3  utils/train_ecapa_tdnn.py --eval --initial_model exps/exp4/model/model_0004.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 5
+python3 utils/train_ecapa_tdnn.py --eval --initial_model exps/exp3/model_0013.model --feat_type fbank --feat_dim 80 --n_cpu 5
 
 conda deactivate
