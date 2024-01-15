531becfa87cabd13d922d7a27f5adf603055926a
miguel <lamahmichelmarie@gmail.com>
2024-01-15 17:51:43 +0100

    15 Janv 2024 17:51

--- recipes/resnet/eval_ecapa.sh
+++ recipes/resnet/eval_ecapa.sh
@@ -3,7 +3,8 @@
 #SBATCH --partition=gpu
 # #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
-#SBATCH --gres=gpu:rtx_3090:1
+# #SBATCH --gres=gpu:rtx_3090:1
+#SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 # #SBATCH --mem=128GB
 #SBATCH --cpus-per-task=5
