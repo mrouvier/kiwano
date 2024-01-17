76cc1245d6ce00ba6e8643b1ccc6fb9ea7356d13
miguel <lamahmichelmarie@gmail.com>
2024-01-17 08:58:11 +0100

    17 Janv 2024 08:58

--- recipes/resnet/process.sh
+++ recipes/resnet/process.sh
@@ -11,23 +11,23 @@
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-rm -rf db
-rm -rf data
+# rm -rf db
+# rm -rf data
 
 #Prepare Voxceleb1
-python3 local/download_voxceleb1.py db/voxceleb1/
-python3 local/prepare_voxceleb1.py db/voxceleb1/ data/voxceleb1/
+#python3 local/download_voxceleb1.py db/voxceleb1/
+#python3 local/prepare_voxceleb1.py db/voxceleb1/ data/voxceleb1/
 
 #Prepare Voxceleb2
-python3 local/download_voxceleb2.py db/voxceleb2/
-python3 local/prepare_voxceleb2.py db/voxceleb2/ data/voxceleb2/
+#python3 local/download_voxceleb2.py db/voxceleb2/
+#python3 local/prepare_voxceleb2.py db/voxceleb2/ data/voxceleb2/
 
 #Prepare MUSAN
-python3 local/download_musan.py db/musan/
-python3 local/prepare_musan.py db/musan/ data/musan/
+#python3 local/download_musan.py db/musan/
+#python3 local/prepare_musan.py db/musan/ data/musan/
 
 #Prepare RIRS NOISES
-python3 local/download_rirs_noises.py db/rirs_noises/
-python3 local/prepare_rirs_noises.py db/musan/ data/rirs_noises
+#python3 local/download_rirs_noises.py db/rirs_noises/
+python3 local/prepare_rirs_noises.py db/rirs_noises/ data/rirs_noises
 
 conda deactivate
