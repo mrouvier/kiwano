122019070a9dd2da1517b38045699e8e2b93b918
miguel <lamahmichelmarie@gmail.com>
2024-01-17 20:40:49 +0100

    17 Janv 2024 20:40

--- recipes/resnet/train_ecapa.sh
+++ recipes/resnet/train_ecapa.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=train_ecapa_kiwano
+#SBATCH --job-name=kf
 #SBATCH --partition=gpu
-# #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
+#SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 # #SBATCH --gres=gpu:nvidia_a100-pcie-40gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 # #SBATCH --gres=gpu:rtx_3090:1
-#SBATCH --gres=gpu:1
+# #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=80GB
-#SBATCH --cpus-per-task=10
-#SBATCH --output=train_ecapa_kiwano_output.log
-#SBATCH --error=train_ecapa_kiwano_error.log
+#SBATCH --mem=20GB
+#SBATCH --cpus-per-task=5
+#SBATCH --output=kf_output.log
+#SBATCH --error=kf_error.log
 
 
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10
+python3 utils/train_ecapa_tdnn.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 5
 
 conda deactivate
--- recipes/resnet/train_wav2vec2.sh
+++ recipes/resnet/train_wav2vec2.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
-#SBATCH --job-name=train_wav2vec2_kiwano
+#SBATCH --job-name=kw
 #SBATCH --partition=gpu
 # #SBATCH --gres=gpu:nvidia_a100-sxm4-80gb:1
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 # #SBATCH --gres=gpu:rtx_3090:1
 #SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=80GB
-#SBATCH --cpus-per-task=10
-#SBATCH --output=train_wav2vec2_kiwano_output.log
-#SBATCH --error=train_wav2vec2_kiwano_error.log
+#SBATCH --mem=20GB
+#SBATCH --cpus-per-task=5
+#SBATCH --output=kw_output.log
+#SBATCH --error=kw_error.log
 
 
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768 --n_cpu 10
+python3 utils/train_ecapa_tdnn.py --save_path exps/exp2 --feat_type wav2vec2 --feat_dim 768 --n_cpu 5
 
 conda deactivate
