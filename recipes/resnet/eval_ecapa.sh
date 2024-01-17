f6a4cf48ca70564fd541d1f61bf60e657b7517a5
miguel <lamahmichelmarie@gmail.com>
2024-01-17 15:27:07 +0100

    17 Janv 2024 15:27

--- recipes/resnet/eval_ecapa.sh
+++ recipes/resnet/eval_ecapa.sh
@@ -5,7 +5,7 @@
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 # #SBATCH --gres=gpu:rtx_3090:1
 #SBATCH --gres=gpu:1
-#SBATCH --time=3-00:00:00
+#SBATCH --time=1-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=4
 #SBATCH --output=eval_ecapa_kiwano_output.log
@@ -16,6 +16,6 @@ source /etc/profile.d/conda.sh
 conda activate kiwano
 
 # python3  utils/train_ecapa_tdnn.py --eval --initial_model exps/exp2/model/model_0017.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 2
-python3 utils/train_ecapa_tdnn.py --eval --initial_model exps/exp1/model_0027.model --feat_type fbank --feat_dim 80 --n_cpu 4
+python3 utils/train_ecapa_tdnn.py --eval --initial_model exps/exp1/model/model_0027.model --feat_type fbank --feat_dim 80 --n_cpu 4
 
 conda deactivate
