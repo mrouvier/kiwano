f49ccce232eaecd4eea21476696ce0fdab659273
miguel <lamahmichelmarie@gmail.com>
2024-01-22 21:35:32 +0100

    22 Janv 2024 21:35

--- recipes/resnet/train_wav2vec2.sh
+++ recipes/resnet/train_wav2vec2.sh
@@ -5,7 +5,7 @@
 # #SBATCH --gres=gpu:tesla_v100-sxm2-32gb:1
 # #SBATCH --gres=gpu:rtx_3090:1
 #SBATCH --gres=gpu:1
-#SBATCH --exclude=apollon,eris,helios
+#SBATCH --exclude=apollon,eris,helios,alpos
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=10
