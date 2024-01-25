0ae6e3fcb52c5bb0ae9a1c1deba220f267e75a18
miguel <lamahmichelmarie@gmail.com>
2024-01-25 22:15:36 +0100

    25 Janv 2024 22:15

--- recipes/resnet/train_wav2vec2.sh
+++ recipes/resnet/train_wav2vec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # job names: kw (base), kw_1 (large), kw_2 (robust), kw_3 (self)
-#SBATCH --job-name=kw_1
+#SBATCH --job-name=kw_2
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_16GB
@@ -17,9 +17,9 @@ conda activate kiwano
 
 # python3 utils/train_ecapa_tdnn.py --save_path exps/exp2_3 --feat_type wav2vec2  --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-960h-lv60-self
 
-# python3 utils/train_ecapa_tdnn.py --save_path exps/exp2_2 --feat_type wav2vec2  --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-robust-ft-libri-960h
+python3 utils/train_ecapa_tdnn.py --save_path exps/exp2_2 --feat_type wav2vec2  --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-robust-ft-libri-960h
 
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp2_1 --feat_type wav2vec2  --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-960h
+# python3 utils/train_ecapa_tdnn.py --save_path exps/exp2_1 --feat_type wav2vec2  --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-960h
 
 # python3 utils/train_ecapa_tdnn.py --save_path exps/exp2 --feat_type wav2vec2 --n_cpu 10 --batch_size 128
 
