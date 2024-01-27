2b8ab81a8223a571b736b13f6a23479acc37e9a0
miguel <lamahmichelmarie@gmail.com>
2024-01-27 04:53:54 +0100

    27 Janv 2024 04:53

--- recipes/resnet/train_ecapa.sh
+++ recipes/resnet/train_ecapa.sh
@@ -4,7 +4,6 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_16GB
-#SBATCH --exclude=alpos
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=10
