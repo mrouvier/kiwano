5eb2b1ad5dda8784db7aff565dda74b11cb3720d
miguel <lamahmichelmarie@gmail.com>
2024-01-27 04:45:54 +0100

    27 Janv 2024 04:45

--- recipes/resnet/train_ecapa.sh
+++ recipes/resnet/train_ecapa.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
-#SBATCH --job-name=kf
+# job name: kf (fbank other) kf_1 (fbank kiwano)
+#SBATCH --job-name=kf_1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --constraint=GPURAM_Max_40GB
+#SBATCH --constraint=GPURAM_Min_16GB
 #SBATCH --exclude=alpos
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
 #SBATCH --cpus-per-task=10
-#SBATCH --output=kf_output.log
-#SBATCH --error=kf_error.log
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
 
 
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
+python3 utils/train_ecapa_tdnn_2.py --save_path exps/exp1_1  --feat_dim 81 --n_cpu 10 --batch_size 128
+# python3 utils/train_ecapa_tdnn.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
 
 conda deactivate
