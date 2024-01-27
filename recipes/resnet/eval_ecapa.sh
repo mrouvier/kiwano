6aae8b5adc9e6aba2e63527d2cb79a62e3991b8b
miguel <lamahmichelmarie@gmail.com>
2024-01-27 17:26:50 +0100

    27 Janv 2024 17:26

--- recipes/resnet/eval_ecapa.sh
+++ recipes/resnet/eval_ecapa.sh
@@ -16,7 +16,7 @@ conda activate kiwano
 
 # python3 -m pdb utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 0
 
-python3  utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 10
+# python3  utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 10
 
 # python3  utils/train_ecapa_tdnn.py --eval --initial_model exps/exp2/model/model_0017.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 5
 # python3 utils/train_ecapa_tdnn.py --eval --initial_model exps/exp1/model/model_0027.model --feat_type fbank --feat_dim 80 --n_cpu 4
--- recipes/resnet/train_ecapa.sh
+++ recipes/resnet/train_ecapa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # job name: kf (fbank other) kf_1 (fbank kiwano)
-#SBATCH --job-name=kf
+#SBATCH --job-name=kf_1
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 #SBATCH --constraint=GPURAM_Min_12GB
@@ -15,7 +15,7 @@
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-# python3 utils/train_ecapa_tdnn_2.py --save_path exps/exp1_1  --feat_dim 81 --n_cpu 10 --batch_size 128
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
+python3 utils/train_ecapa_tdnn_2.py --save_path exps/exp1_1  --feat_dim 81 --n_cpu 10 --batch_size 128
+# python3 utils/train_ecapa_tdnn.py --save_path exps/exp1 --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
 
 conda deactivate
