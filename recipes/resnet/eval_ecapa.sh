4b5e58c8fce2b212b28aab9c56ebc45005c0e213
miguel <lamahmichelmarie@gmail.com>
2024-01-27 17:08:00 +0100

    27 Janv 2024 17:07

--- recipes/resnet/eval_ecapa.sh
+++ recipes/resnet/eval_ecapa.sh
@@ -6,7 +6,7 @@
 #SBATCH --exclude=apollon,eris
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=16GB
-#SBATCH --cpus-per-task=10
+# #SBATCH --cpus-per-task=10
 #SBATCH --output=%x_output.log
 #SBATCH --error=%x_error.log
 
@@ -14,9 +14,9 @@
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-# python3 -m pdb utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 0
+python3 -m pdb utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 0
 
-python3  utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 10
+# python3  utils/train_ecapa_tdnn_2.py --eval --initial_model exps/exp1_1/model/model_0001.model  --feat_dim 81 --n_cpu 10
 
 # python3  utils/train_ecapa_tdnn.py --eval --initial_model exps/exp2/model/model_0017.model  --feat_type wav2vec2 --feat_dim 768 --n_cpu 5
 # python3 utils/train_ecapa_tdnn.py --eval --initial_model exps/exp1/model/model_0027.model --feat_type fbank --feat_dim 80 --n_cpu 4
