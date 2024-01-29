0c4ad5c33589a84d659cb00e74fb0cd8014f0580
miguel <lamahmichelmarie@gmail.com>
2024-01-29 10:31:08 +0100

    29 Janv 2024 10:31

---
+++ recipes/resnet/train_ecapa_ddp.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#SBATCH --job-name=kf_1_ddp
+# #SBATCH --nodes=2
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:2
+#SBATCH --constraint=GPURAM_Max_12GB
+#SBATCH --time=7-00:00:00
+#SBATCH --mem=16GB
+#SBATCH --cpus-per-task=8
+#SBATCH --output=%x_output.log
+#SBATCH --error=%x_error.log
+
+
+source /etc/profile.d/conda.sh
+conda activate ecapa_tdnn
+
+python3 utils/train_ecapa_tdnn_2.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128
+
+conda deactivate
