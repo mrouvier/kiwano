987fba722381c70951e46faac1bdd3a4df4672dc
miguel <lamahmichelmarie@gmail.com>
2024-01-29 10:40:24 +0100

    29 Janv 2024 10:40

--- recipes/resnet/train_ecapa_ddp.sh
+++ recipes/resnet/train_ecapa_ddp.sh
@@ -14,6 +14,6 @@
 source /etc/profile.d/conda.sh
 conda activate ecapa_tdnn
 
-python3 utils/train_ecapa_tdnn_2.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128
+python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128
 
 conda deactivate
