2a56e5b15a1d7d415211d4107a04e37c8ba8e4fe
miguel <lamahmichelmarie@gmail.com>
2024-01-30 19:18:18 +0100

    30 Janv 2024 19:18

--- recipes/resnet/train_ecapa_ddp.sh
+++ recipes/resnet/train_ecapa_ddp.sh
@@ -5,7 +5,7 @@
 #SBATCH --gres=gpu:2
 #SBATCH --constraint=GPURAM_Max_16GB
 #SBATCH --time=7-00:00:00
-#SBATCH --mem=32GB
+#SBATCH --mem=64GB
 #SBATCH --cpus-per-task=10
 #SBATCH --output=%x_output.log
 #SBATCH --error=%x_error.log
@@ -14,6 +14,6 @@
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 64
+python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128
 
 conda deactivate
