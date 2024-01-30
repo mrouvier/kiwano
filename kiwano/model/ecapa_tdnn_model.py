20d01ed682b9889c17ceb9e364b37a9c5b3e61a6
miguel <lamahmichelmarie@gmail.com>
2024-01-30 12:38:04 +0100

    30 Janv 2024 12:38

--- kiwano/model/ecapa_tdnn_model.py
+++ kiwano/model/ecapa_tdnn_model.py
@@ -513,7 +513,7 @@ class ECAPAModelDDP(nn.Module):
 
     def load_parameters(self, path):
         self_state = self.state_dict()
-        loaded_state = torch.load(path, map_location="cpu")
+        loaded_state = torch.load(path, map_location="gpu")
         for name, param in loaded_state.items():
             origname = name
             if name not in self_state:
@@ -870,7 +870,7 @@ class ECAPAModel2DDP(nn.Module):
 
     def load_parameters(self, path):
         self_state = self.state_dict()
-        loaded_state = torch.load(path, map_location="cpu")
+        loaded_state = torch.load(path, map_location="gpu")
         for name, param in loaded_state.items():
             origname = name
             if name not in self_state:
