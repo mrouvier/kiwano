3dd41425b464fb0fbad9773559eeebc08fd2f3b0
miguel <lamahmichelmarie@gmail.com>
2024-01-31 09:44:53 +0100

    31 Janv 2024 09:44

--- kiwano/model/ecapa_tdnn_model.py
+++ kiwano/model/ecapa_tdnn_model.py
@@ -620,16 +620,7 @@ class ECAPAModel2(nn.Module):
         sys.stdout.flush()
 
         emb_dataset = EmbeddingsDataset2(setfiles, eval_path)
-        # emb_loader = DataLoader(emb_dataset, batch_size=100, num_workers=n_cpu, collate_fn=collate_fn)
-        emb_loader = DataLoader(
-            emb_dataset,
-            batch_size=32,
-            shuffle=False,
-            sampler=DistributedSampler(emb_dataset, shuffle=False),
-            num_workers=n_cpu,
-            drop_last=True,
-            collate_fn=collate_fn
-        )
+        emb_loader = DataLoader(emb_dataset, batch_size=100, num_workers=n_cpu, collate_fn=collate_fn)
         for idx, batch in tqdm.tqdm(enumerate(emb_loader, start=1), total=len(emb_loader)):
             all_file, all_data_1, all_lengths_1, all_data_2 = batch
             for i in range(len(all_file)):
@@ -814,7 +805,16 @@ class ECAPAModel2DDP(nn.Module):
 
         self.print_info("BEGIN embeddings")
         emb_dataset = EmbeddingsDataset2(setfiles, eval_path)
-        emb_loader = DataLoader(emb_dataset, batch_size=32, num_workers=n_cpu, collate_fn=collate_fn)
+        # emb_loader = DataLoader(emb_dataset, batch_size=32, num_workers=n_cpu, collate_fn=collate_fn)
+        emb_loader = DataLoader(
+            emb_dataset,
+            batch_size=32,
+            shuffle=False,
+            sampler=DistributedSampler(emb_dataset, shuffle=False),
+            num_workers=n_cpu,
+            drop_last=True,
+            collate_fn=collate_fn
+        )
         for idx, batch in tqdm.tqdm(enumerate(emb_loader, start=1), total=len(emb_loader), disable=self.disable_tqdm):
             all_file, all_data_1, all_lengths_1, all_data_2 = batch
             for i in range(len(all_file)):
