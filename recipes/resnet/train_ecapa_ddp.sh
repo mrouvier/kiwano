f3dd9973875726792808f792debf14fccd7ac4b8
miguel <lamahmichelmarie@gmail.com>
2024-02-02 00:59:37 +0100

    02 Fev 2024 00:59

--- recipes/resnet/train_ecapa_ddp.sh
+++ recipes/resnet/train_ecapa_ddp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # job name: kf_ddp (fbank other) kf_1_ddp (fbank kiwano)
-#SBATCH --job-name=kf_ddp
+#SBATCH --job-name=kf_1_ddp
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
-#SBATCH --constraint=GPURAM_Max_16GB
+#SBATCH --gres=gpu:2
+# #SBATCH --constraint=GPURAM_Max_16GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=64GB
 #SBATCH --cpus-per-task=10
@@ -14,9 +14,9 @@
 source /etc/profile.d/conda.sh
 conda activate kiwano
 
-python3  utils/train_ecapa_tdnn_ddp.py --eval --initial_model exps/exp1_ddp/model/model_0006.model  --feat_type fbank --feat_dim 80 --n_cpu 10
+# python3  utils/train_ecapa_tdnn_ddp.py --eval --initial_model exps/exp1_ddp/model/model_0006.model  --feat_type fbank --feat_dim 80 --n_cpu 10
 # python3  utils/train_ecapa_tdnn_2_ddp.py --eval --initial_model exps/exp1_1_ddp/model/model_0006.model  --feat_dim 81 --n_cpu 10
-# python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128
+python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128
 # python3 utils/train_ecapa_tdnn_ddp.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128
 
 conda deactivate
