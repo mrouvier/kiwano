9ed1cea21f8bf2a3c37fe2a48a4a44ef009394e1
miguel <lamahmichelmarie@gmail.com>
2024-02-04 02:06:01 +0100

    04 Fev 2024 02:05

--- recipes/resnet/train_ecapa_ddp.sh
+++ recipes/resnet/train_ecapa_ddp.sh
@@ -2,7 +2,7 @@
 # job name: kf_ddp (fbank other) kf_1_ddp (fbank kiwano)
 #SBATCH --job-name=kf_ddp
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:3
+#SBATCH --gres=gpu:2
 #SBATCH --constraint=GPURAM_Max_16GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=64GB
