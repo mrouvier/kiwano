429010e83e9cee57eafd2ae5af5fc4cdcc381587
miguel <lamahmichelmarie@gmail.com>
2024-02-17 10:01:36 +0100

    17 Fev 2024 10:01

--- recipes/resnet/train_ecapa_ddp.sh
+++ recipes/resnet/train_ecapa_ddp.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # job name: kf_ddp (fbank other) kf_1_ddp (fbank kiwano)
-#SBATCH --job-name=kf_ddp
+#SBATCH --job-name=kf_1_ddp
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:2
-#SBATCH --constraint=GPURAM_Max_24GB
+#SBATCH --constraint=GPURAM_Min_16GB&GPURAM_Max_24GB
 #SBATCH --exclude=beroe,calypso
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=64GB
@@ -17,7 +17,7 @@ conda activate kiwano
 
 # python3  utils/train_ecapa_tdnn_ddp.py --eval --initial_model exps/exp1_ddp/model/model_0006.model  --feat_type fbank --feat_dim 80 --n_cpu 10
 # python3  utils/train_ecapa_tdnn_2_ddp.py --eval --initial_model exps/exp1_1_ddp/model/model_0006.model  --feat_dim 81 --n_cpu 10
-# python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128 --master_port 54321
-python3 utils/train_ecapa_tdnn_ddp.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128 --master_port 54322
+python3 utils/train_ecapa_tdnn_2_ddp.py --save_path exps/exp1_1_ddp  --feat_dim 81 --n_cpu 10 --batch_size 128 --master_port 54321
+# python3 utils/train_ecapa_tdnn_ddp.py --save_path exps/exp1_ddp --feat_type fbank --feat_dim 80 --n_cpu 10 --batch_size 128 --master_port 54322
 
 conda deactivate
