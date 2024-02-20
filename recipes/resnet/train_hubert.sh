c247fda4ff46401eb6f39f1fb7d32cdc3473c106
miguel <lamahmichelmarie@gmail.com>
2024-02-20 14:09:13 +0100

    20 Fev 2024 14:09

--- recipes/resnet/train_hubert.sh
+++ recipes/resnet/train_hubert.sh
@@ -9,7 +9,7 @@
 #SBATCH --job-name=khb_ls960
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
-#SBATCH --constraint=GPURAM_Max_16GB
+#SBATCH --constraint=GPURAM_Min_12GB&GPURAM_Max_16GB
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=10
