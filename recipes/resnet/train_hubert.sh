7fb86cdcd8eecc28d17d908e439b3c38315b8a7a
miguel <lamahmichelmarie@gmail.com>
2024-02-20 14:34:11 +0100

    20 Fev 2024 14:34

--- recipes/resnet/train_hubert.sh
+++ recipes/resnet/train_hubert.sh
@@ -27,8 +27,8 @@ conda activate kiwano
 
 # python3 utils/train_ecapa_tdnn.py --save_path exps/exp_hubert_large_ls960_ft --feat_type hubert --n_cpu 10 --batch_size 128 --model_name facebook/hubert-large-ls960-ft
 
-# python3 utils/train_ecapa_tdnn.py --save_path exps/exp_hubert_xlarge_ls960_ft --feat_type hubert --n_cpu 10 --batch_size 128 --model_name  facebook/hubert-xlarge-ls960-ft
+python3 utils/train_ecapa_tdnn.py --save_path exps/exp_hubert_xlarge_ls960_ft --feat_type hubert --n_cpu 10 --batch_size 128 --model_name  facebook/hubert-xlarge-ls960-ft
 
-python3  utils/train_ecapa_tdnn.py --save_path exps/exp_hubert_base_ls960 --feat_type hubert --n_cpu 10 --batch_size 64 --model_name facebook/hubert-base-ls960
+# python3  utils/train_ecapa_tdnn.py --save_path exps/exp_hubert_base_ls960 --feat_type hubert --n_cpu 10 --batch_size 64 --model_name facebook/hubert-base-ls960
 
 conda deactivate
