9d44034c47a615ef7ab1d1bc16bcc8fbdbabadf1
miguel <lamahmichelmarie@gmail.com>
2024-02-20 14:46:38 +0100

    20 Fev 2024 14:46

--- kiwano/model/hubert.py
+++ kiwano/model/hubert.py
@@ -34,7 +34,7 @@ class CustomHuBERTModel(nn.Module):
         return result
 
     def get_output_dim(self):
-        x = [torch.randn(16_000)]
+        x = torch.randn(1, 16_000)
         with torch.no_grad():
             x = self.processor(x, return_tensor='pt', sampling_rate=16_000)
             x = x.input_values[0]
--- kiwano/model/wavlm.py
+++ kiwano/model/wavlm.py
@@ -33,10 +33,10 @@ class CustomWavLMModel(nn.Module):
         return result
 
     def get_output_dim(self):
-        x = torch.randn(16_000)
+        x = torch.randn(1, 16_000)
         with torch.no_grad():
             x = self.processor(x, return_tensor='pt', sampling_rate=16_000)
-            x = x.input_values
+            x = x.input_values[0]
             x = torch.tensor(x).to(self.device)
             output = self.model(x)
 
