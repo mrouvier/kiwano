619ec1207b4b1f9d3d7cb4804755ebc2c6df872e
miguel <lamahmichelmarie@gmail.com>
2024-02-24 23:28:07 +0100

    24 Fev 2024 23:28

--- recipes/resnet/process.sh
+++ recipes/resnet/process.sh
@@ -4,7 +4,7 @@
 ##SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=64GB
-#SBATCH --cpus-per-task=20
+# #SBATCH --cpus-per-task=20
 #SBATCH --output=%x_output.log
 #SBATCH --error=%x_error.log
 
