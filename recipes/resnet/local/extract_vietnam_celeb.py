65c905610138c5c1bee1c8f1e1f8c554417a82e3
miguel <lamahmichelmarie@gmail.com>
2024-02-25 09:14:08 +0100

    25 Fev 2024 09:14

--- recipes/resnet/local/extract_vietnam_celeb.py
+++ recipes/resnet/local/extract_vietnam_celeb.py
@@ -19,11 +19,9 @@ def extract_vietnam_celeb(target_dir: Pathlike = "."):
     #              'vietnam-celeb-part.z01',
     #              'vietnam-celeb-part.z02',
     #              'vietnam-celeb-part.z03']
-    zip_files = ['vietnam-celeb-part.zip']
-    with open(zip_path, "wb") as outFile:
-        for file in tqdm(zip_files):
-            path = target_dir / file
-            with open(path, "rb") as inFile:
+    with open(zip_name, "wb") as outFile:
+        for file in tqdm(sorted(target_dir.glob("vietnam-celeb-part.zip"))):
+            with open(file, "rb") as inFile:
                 shutil.copyfileobj(inFile, outFile)
     print(f"Unzipping files...", flush=True)
     with zipfile.ZipFile(zip_path) as zf:
