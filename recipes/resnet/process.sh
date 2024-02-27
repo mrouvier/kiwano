5c405b12a0e34cad8c499d3363ed91d467aa20b6
miguel <lamahmichelmarie@gmail.com>
2024-02-27 11:33:05 +0100

    27 Fev 2024 11:32

--- recipes/resnet/process.sh
+++ recipes/resnet/process.sh
@@ -61,9 +61,9 @@ python3 local/prepare_voxceleb2.py --in_data db/voxceleb2/ --old_file train_list
 
 # python3 local/prepare_cn_celeb.py --in_data db/cn_celeb/CN-Celeb_flac/eval/lists/ --old_file trials.lst
 
-python3 local/prepare_cn_celeb.py --in_data db/cn_celeb/CN-Celeb2_flac --old_file liste.edited
+# python3 local/prepare_cn_celeb.py --in_data db/cn_celeb/CN-Celeb2_flac --old_file liste.edited
 
-# python3 local/prepare_cn_celeb.py --in_data data/cn_celeb/ --old_file liste --out_data db/cn_celeb/CN-Celeb2_flac/
+python3 local/prepare_cn_celeb.py --in_data data/cn_celeb/ --old_file liste --out_data db/cn_celeb/CN-Celeb2_flac/
 
 # python3 local/extract_vietnam_celeb.py db/vietnam_celeb/
 # rm  db/vietnam_celeb/vietnam-celeb.zip
@@ -85,10 +85,10 @@ python3 local/prepare_cn_celeb.py --in_data db/cn_celeb/CN-Celeb2_flac --old_fil
 #rm -r data/vietnam_celeb/
 #python3 local/prepare_vietnam_celeb.py db/vietnam_celeb/ data/vietnam_celeb/
 
-# python3 local/prepare_vietnam_celeb.py --in_data db/vietnam_celeb/ --out_data db/vietnam_celeb/ --old_file vietnam-celeb-t.txt
+python3 local/prepare_vietnam_celeb.py --in_data db/vietnam_celeb/ --out_data db/vietnam_celeb/ --old_file vietnam-celeb-t.txt
 
 # python3 local/prepare_vietnam_celeb.py --in_data db/vietnam_celeb/ --old_file vietnam-celeb-t.txt
 
-python3 local/prepare_vietnam_celeb.py --in_data db/vietnam_celeb/ --old_file vietnam-celeb-t.txt.edited
+# python3 local/prepare_vietnam_celeb.py --in_data db/vietnam_celeb/ --old_file vietnam-celeb-t.txt.edited
 
 conda deactivate
