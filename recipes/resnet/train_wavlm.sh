d3a7d30af37aa8e396ab294a67123b2a5ca7b77e
miguel <lamahmichelmarie@gmail.com>
2024-03-02 00:29:19 +0100

    02 Mars 2024 00:29

--- recipes/resnet/train_wavlm.sh
+++ recipes/resnet/train_wavlm.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 # #SBATCH --constraint=GPURAM_Min_12GB
-#SBATCH --constraint=GPURAM_Min_32GB
-#SBATCH --nodelist=eris
+# #SBATCH --constraint=GPURAM_Min_32GB
+#SBATCH --nodelist=hemera,heracles,orion
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=16
@@ -27,7 +27,7 @@ conda activate kiwano
 
 # python3 utils/train_ecapa_tdnn.py --save_path exps/exp_wavlm_base --feat_type wavlm --n_cpu 10 --batch_size 128 --model_name microsoft/wavlm-base
 
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp_wavlm_base_ddp --feat_type wavlm --n_cpu 16 --batch_size 512 --model_name microsoft/wavlm-base --max_epoch 100
+python3 utils/train_ecapa_tdnn.py --save_path exps/exp_wavlm_base_ddp --feat_type wavlm --n_cpu 16 --batch_size 256 --model_name microsoft/wavlm-base --max_epoch 100
 
 # python3 -m pdb utils/train_ecapa_tdnn.py --save_path exps/exp_wavlm_base --feat_type wavlm --n_cpu 10 --batch_size 128 --model_name microsoft/wavlm-base
 
