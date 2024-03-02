1708de91db9401b14e6ea68941bc2c96124ed1bf
miguel <lamahmichelmarie@gmail.com>
2024-03-02 01:13:32 +0100

    02 Mars 2024 01:13

--- recipes/resnet/train_wav2vec2.sh
+++ recipes/resnet/train_wav2vec2.sh
@@ -4,8 +4,8 @@
 #SBATCH --partition=gpu
 #SBATCH --gres=gpu:1
 # #SBATCH --constraint=GPURAM_Min_12GB
-#SBATCH --constraint=GPURAM_Min_32GB
-#SBATCH --nodelist=eris
+#SBATCH --constraint=GPURAM_24GB
+# #SBATCH --nodelist=eris
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 #SBATCH --cpus-per-task=16
@@ -22,6 +22,6 @@ conda activate kiwano
 
 # python3 utils/train_ecapa_tdnn.py --save_path exps/exp2_1 --feat_type wav2vec2  --n_cpu 10 --batch_size 128 --model_name facebook/wav2vec2-large-960h
 
-python3 utils/train_ecapa_tdnn.py --save_path exps/exp2 --feat_type wav2vec2 --n_cpu 16 --batch_size 512 --max_epoch 100
+python3 utils/train_ecapa_tdnn.py --save_path exps/exp2 --feat_type wav2vec2 --n_cpu 16 --batch_size 256 --max_epoch 100
 
 conda deactivate
