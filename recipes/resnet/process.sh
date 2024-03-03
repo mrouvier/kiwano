ac52f4426133f6ad440855e2a6865cc9fc6bd76a
miguel <lamahmichelmarie@gmail.com>
2024-03-03 13:31:45 +0100

    03 Mars 2024 13:31

--- recipes/resnet/process.sh
+++ recipes/resnet/process.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=nist
 ##SBATCH --partition=gpu
-#SBATCH --gres=gpu:1
+##SBATCH --gres=gpu:1
 #SBATCH --time=7-00:00:00
 #SBATCH --mem=32GB
 ##SBATCH --cpus-per-task=8
