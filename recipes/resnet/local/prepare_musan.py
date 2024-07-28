c5944cb5c8edcec5ff2c732ce8dd33d64c23bee8
mrouvier <mickael.rouvier@gmail.com>
2024-07-28 16:23:21 +0200

    add downsampling

--- recipes/resnet/local/prepare_musan.py
+++ recipes/resnet/local/prepare_musan.py
@@ -6,7 +6,8 @@ from pathlib import Path
 import torchaudio
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
-
+from subprocess import PIPE, run
+import os
 
 import argparse
 
@@ -14,14 +15,26 @@ def get_duration(file_path: str):
    info = torchaudio.info(file_path)
    return info.num_frames/info.sample_rate
 
-def process_file(segment):
-    duration = str(round(float(get_duration(segment)),2))
+def process_file(segment: Pathlike, sampling_frequency: int, out_data: Pathlike):
+
+    new_segment = segment
+
+    if sampling_frequency != 16000:
+        out = Path( out_data / str(segment).split("/")[-4]  / str(segment).split("/")[-3] / str(segment).split("/")[-2]  )
+        out.mkdir(parents=True, exist_ok=True)
+
+        output = Path( out_data / str(segment).split("/")[-4]  / str(segment).split("/")[-3] / str(segment).split("/")[-2] /  str(segment).split("/")[-1]  )
+        cmd = "ffmpeg -y -threads 1 -i " + str(segment) + " -acodec pcm_s16le -ac 1 -ar " + str(sampling_frequency) + " -ab 48 -threads 1 " + str(output)
+        proc = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        new_segment = output
+
+    duration = str(round(float(get_duration(str(new_segment))),2))
     name = str(segment).split("/")[-1].split(".")[0]
     spkid = str(segment).split("/")[-3]
 
-    return name, spkid, duration, segment
+    return name, spkid, duration, new_segment
 
-def prepare_musan(in_data: Pathlike, out_data: Pathlike, num_jobs: int):
+def prepare_musan(in_data: Pathlike, out_data: Pathlike, sampling_frequency: int, num_jobs: int):
     in_data = Path(in_data)
 
     out_data = Path(out_data)
@@ -35,7 +48,7 @@ def prepare_musan(in_data: Pathlike, out_data: Pathlike, num_jobs: int):
         futures = []
 
         for segment in wav_lst:
-            futures.append( ex.submit(process_file, segment) )
+            futures.append( ex.submit(process_file, segment, sampling_frequency, out_data) )
 
         for future in tqdm(futures, total=len(futures), desc=f"Processing MUSAN..."):
             name, spkid, duration, segment = future.result()
@@ -51,7 +64,12 @@ if __name__ == '__main__':
                         help='the path to the target directory where the wav files are stored')
     parser.add_argument('out_data', metavar="out_data", type=str,
                         help='the path to the target directory where the liste will be stored')
+    parser.add_argument('--thread', type=int, default=20,
+            help='Number of parallel jobs (default: 20)')
+    parser.add_argument('--downsampling', type=int, default=16000,
+                        help='the value of sampling frequency (default: 16000)')
+
 
     args = parser.parse_args()
 
-    prepare_musan(args.in_data, args.out_data, 20)
+    prepare_musan(args.in_data, args.out_data, args.downsampling, args.thread)
--- recipes/resnet/local/prepare_rirs_noises.py
+++ recipes/resnet/local/prepare_rirs_noises.py
@@ -5,6 +5,9 @@ from kiwano.utils import Pathlike, get_all_files
 from pathlib import Path
 import torchaudio
 from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
+from subprocess import PIPE, run
+import os
 
 import argparse
 
@@ -13,7 +16,29 @@ def get_duration(file_path: str):
    return info.num_frames/info.sample_rate
 
 
-def prepare_rirs_noises(in_data: Pathlike, out_data: Pathlike):
+
+def process_file(segment: Pathlike, sampling_frequency: int, out_data: Pathlike):
+
+    new_segment = segment
+
+    if sampling_frequency != 16000:
+        out = Path( out_data / str(segment).split("/")[-5]  /  str(segment).split("/")[-4]  / str(segment).split("/")[-3] / str(segment).split("/")[-2] )
+        out.mkdir(parents=True, exist_ok=True)
+
+        output = Path( out_data / str(segment).split("/")[-5]  /  str(segment).split("/")[-4]  / str(segment).split("/")[-3] / str(segment).split("/")[-2] /  str(segment).split("/")[-1]  )
+        cmd = "ffmpeg -y -threads 1 -i " + str(segment) + " -acodec pcm_s16le -ac 1 -ar " + str(sampling_frequency) + " -ab 48 -threads 1 " + str(output)
+        proc = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        new_segment = output
+
+    duration = str(round(float(get_duration(str(new_segment))),2))
+    name = str(segment).split("/")[-1].split(".")[0]
+    spkid = str(segment).split("/")[-3]
+
+    return spkid+"_"+name, spkid, duration, new_segment
+
+
+
+def prepare_rirs_noises(in_data: Pathlike, out_data: Pathlike, sampling_frequency: int, num_jobs: int):
     in_data = Path(in_data)
 
     out_data = Path(out_data)
@@ -23,12 +48,18 @@ def prepare_rirs_noises(in_data: Pathlike, out_data: Pathlike):
 
     wav_lst = get_all_files(in_data, match_and=[".wav"], match_or=["mediumroom", "smallroom"])
 
-    for segment in tqdm(wav_lst):
-        name = str(segment).split("/")[-1].split(".")[0]
-        room = str(segment).split("/")[-3]
-        if room == "mediumroom" or room == "smallroom":
-            duration = str(round(float(get_duration(segment)),2))
-            liste.write(f"{room}_{name} {room} {duration} {segment}\n")
+
+    with ProcessPoolExecutor(num_jobs) as ex:
+        futures = []
+
+        for segment in wav_lst:
+            room = str(segment).split("/")[-3]
+            if room == "mediumroom" or room == "smallroom":
+                futures.append( ex.submit(process_file, segment, sampling_frequency, out_data) )
+
+        for future in tqdm(futures, total=len(futures), desc=f"Processing MUSAN..."):
+            name, spkid, duration, segment = future.result()
+            liste.write(f"{name} {spkid} {duration} {segment}\n")
 
     liste.close()
 
@@ -40,7 +71,14 @@ if __name__ == '__main__':
                         help='the path to the target directory where the wav files are stored')
     parser.add_argument('out_data', metavar="out_data", type=str,
                         help='the path to the target directory where the liste will be stored')
+    parser.add_argument('--thread', type=int, default=20,
+            help='Number of parallel jobs (default: 20)')
+    parser.add_argument('--downsampling', type=int, default=16000,
+                        help='the value of sampling frequency (default: 16000)')
+
+
+
 
     args = parser.parse_args()
 
-    prepare_rirs_noises(args.in_data, args.out_data)
+    prepare_rirs_noises(args.in_data, args.out_data, args.downsampling, args.thread)
