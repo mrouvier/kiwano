3118e7a8dbb62b26ec8cc5c1a1ca10c731b3c741
mrouvier <mickael.rouvier@gmail.com>
2024-08-09 15:41:21 +0200

    modified compute_score script

--- recipes/resnet/compute_score.sh
+++ recipes/resnet/compute_score.sh
@@ -7,16 +7,26 @@ dir=$1
 #dir=exp/resnet_v2h_jeffrey_fbank96/
 #exp/resnet_v2h/
 
-python3 utils/compute_cosine.py  data/voxceleb1/voxceleb1-e-cleaned.trials  "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |"   "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |" > $dir/voxceleb1.${2}/scores.txt
-python3 utils/compute_eer.py data/voxceleb1/voxceleb1-e-cleaned.trials $dir/voxceleb1.${2}/scores.txt
-python3 utils/compute_dcf.py data/voxceleb1/voxceleb1-e-cleaned.trials $dir/voxceleb1.${2}/scores.txt
-python3 utils/compute_cllr.py data/voxceleb1/voxceleb1-e-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+
+echo "voxceleb1-o"
+python utils/compute_cosine.py  data/voxceleb1/voxceleb1-o-cleaned.trials  "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |"   "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |" > $dir/voxceleb1.${2}/scores.txt
+python utils/compute_eer.py data/voxceleb1/voxceleb1-o-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+python utils/compute_dcf.py data/voxceleb1/voxceleb1-o-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+python utils/compute_cllr.py data/voxceleb1/voxceleb1-o-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+echo -e "\n"
 
 
+echo "voxceleb1-e"
+python utils/compute_cosine.py  data/voxceleb1/voxceleb1-e-cleaned.trials  "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |"   "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |" > $dir/voxceleb1.${2}/scores.txt
+python utils/compute_eer.py data/voxceleb1/voxceleb1-e-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+python utils/compute_dcf.py data/voxceleb1/voxceleb1-e-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+python utils/compute_cllr.py data/voxceleb1/voxceleb1-e-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+echo -e "\n"
 
 
-python3 utils/compute_cosine.py  data/voxceleb1/voxceleb1-h-cleaned.trials  "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |"   "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |" > $dir/voxceleb1.${2}/scores.txt
-python3 utils/compute_eer.py data/voxceleb1/voxceleb1-h-cleaned.trials $dir/voxceleb1.${2}/scores.txt
-python3 utils/compute_dcf.py data/voxceleb1/voxceleb1-h-cleaned.trials $dir/voxceleb1.${2}/scores.txt
-python3 utils/compute_cllr.py data/voxceleb1/voxceleb1-h-cleaned.trials $dir/voxceleb1.${2}/scores.txt
-
+echo "voxceleb1-h"
+python utils/compute_cosine.py  data/voxceleb1/voxceleb1-h-cleaned.trials  "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |"   "pkl:cat $dir/voxceleb1.${2}/xvector.*.pkl |" > $dir/voxceleb1.${2}/scores.txt
+python utils/compute_eer.py data/voxceleb1/voxceleb1-h-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+python utils/compute_dcf.py data/voxceleb1/voxceleb1-h-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+python utils/compute_cllr.py data/voxceleb1/voxceleb1-h-cleaned.trials $dir/voxceleb1.${2}/scores.txt
+echo -e "\n"
