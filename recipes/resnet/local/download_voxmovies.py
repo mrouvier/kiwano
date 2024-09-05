e493c0004d76582ac6fafe41ecad32d58420c914
Nicolas bataille <63408806+NicolasBataille@users.noreply.github.com>
2024-09-05 16:18:01 +0200

    Updated download script to remove macos metadata files. Added preparation script.

--- recipes/resnet/local/download_voxmovies.py
+++ recipes/resnet/local/download_voxmovies.py
@@ -16,6 +16,17 @@ VOXMOVIES_URL = [
     ["https://drive.google.com/uc?export=download&id=1K-CSOE3IJyhaq--wkj1RhZIMND4qdgri&authuser=1&confirm=t", "de90dc74430a1f65157d9b5505da7799"]
 ]
 
+def delete_dot_underscore_files(directory: Path, verbose: bool = False):
+    for root, dirs, files in os.walk(directory):
+        for file in files:
+            if file.startswith("._"):
+                file_path = Path(root) / file
+                try:
+                    file_path.unlink() 
+                    if verbose:
+                        logging.info(f"Deleted {file_path}")
+                except Exception as e:
+                    logging.error(f"Failed to delete {file_path}: {e}")
 
 def download_voxmovies(target_dir: Pathlike = ".", force_download: Optional[bool] = False, do_check_md5: Optional[bool] = False, jobs: int = 10):
     target_dir = Path(target_dir)
@@ -31,8 +42,10 @@ def download_voxmovies(target_dir: Pathlike = ".", force_download: Optional[bool
             gdrive_download(url[0], str(zip_path))
 
         logging.info("Unzipping VoxMovies...")
-        extract_tar(zip_path, target_dir)
-        os.remove(zip_path)
+        extract_tar(tar_gz_path=zip_path, extract_path=target_dir)
+
+        logging.info("Cleaning up metadata files...")
+        delete_dot_underscore_files(target_dir)
 
     if do_check_md5:
         check_md5(target_dir, VOXMOVIES_URL)
---
+++ recipes/resnet/local/prepare_voxmovies.py
@@ -0,0 +1,104 @@
+#!/usr/bin/python3
+
+import sys
+from kiwano.utils import Pathlike, get_all_files
+from pathlib import Path
+import torchaudio
+from tqdm import tqdm
+from concurrent.futures import ProcessPoolExecutor
+
+from subprocess import PIPE, run
+
+import argparse
+import os
+
+def get_duration(file_path: str):
+   info = torchaudio.info(file_path)
+   return info.num_frames/info.sample_rate 
+
+def process_file(segment: Pathlike, sampling_frequency: int, in_data: Pathlike):
+    name = "_".join(str(segment).split("/")[-3:]).split(".")[0]
+    spkid = str(segment).split("/")[-3]
+    ref = str(segment).split("/")[-2]
+    n = str(segment).split("/")[-1]
+    duration = str(round(float(get_duration(segment)),2))
+
+    if sampling_frequency != 16000:
+        name_dir = "wav"
+        out = Path(in_data / name_dir / spkid / ref)
+        out.mkdir(parents=True, exist_ok=True)
+
+        output = str(Path(in_data / name_dir / spkid / ref / n))
+        output = Path(output)
+
+        cmd = "ffmpeg -threads 1 -i " + str(segment) + " -acodec pcm_s16le -ac 1 -ar " + str(sampling_frequency) + " -ab 48 -threads 1 " + str(output)
+        proc = run(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        segment = output
+
+    return name, spkid, duration, segment
+
+
+def prepare_voxmovies(in_data: Pathlike, out_data: Pathlike, jobs: int, sampling_frequency: int, delete_zip: bool):
+    in_data = Path(in_data)
+
+    out_data = Path(out_data)
+    out_data.mkdir(parents=True, exist_ok=True)
+
+    nameListe = "liste"
+
+    liste = open(out_data / nameListe, "w")
+
+    test_lst = get_all_files(in_data / "voxmovies_test", match_and=[".wav"])
+    train_lst = get_all_files(in_data / "voxmovies_train", match_and=[".wav"])
+
+    with ProcessPoolExecutor(jobs) as ex:
+        futures = []
+
+        for segment in test_lst:
+            futures.append(ex.submit(process_file, segment, sampling_frequency, in_data))
+        for segment in train_lst:
+            futures.append(ex.submit(process_file, segment, sampling_frequency, in_data))
+
+        for future in tqdm(futures, total=len(futures), desc="Processing files"):
+            name, spkid, duration, segment = future.result()
+            liste.write(f"{name} {spkid} {segment} {duration}\n")
+
+    liste.close()
+
+    for txt, trials in zip( ["e1.txt", "e2.txt", "e3.txt", "e4.txt", "e5.txt"], ["voxmovies-e1.trials", "voxmovies-e2.trials", "voxmovies-e3.trials", "voxmovies-e4.trials", "voxmovies-e5.trials"]):
+        r_txt = open(in_data / 'evaluation_sets' / txt, "r")
+        w_trials = open(out_data / trials, "w")
+
+        for line in r_txt:
+            line = line.strip().split(" ")
+
+            if line[0] == "0":
+                line[0] = "nontarget"
+            else:
+                line[0] = "target"
+            
+            w_trials.write(line[1].replace("/", "_").split(".")[0]+" "+line[2].replace("/", "_").split(".")[0]+" "+line[0]+"\n")
+        
+        w_trials.close()
+        r_txt.close()
+    
+    if delete_zip:
+        os.remove(in_data / "vox_movies.tar.gz")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--thread', type=int, default=10,
+                    help='Number of parallel jobs (default: 10)')
+    parser.add_argument('in_data', type=str,
+                    help='Path to the directory containing the "wav" directory')
+    parser.add_argument('out_data', type=str,
+                    help='Path to the target directory where the list will be stored')
+    parser.add_argument('--downsampling', type=int, default=16000,
+                    help='Downsampling frequency value (default: 16000)')
+    parser.add_argument('--deleteZIP', action='store_true', default=False,
+                    help='Delete the already extracted ZIP files (default: False)')
+
+    args = parser.parse_args()
+
+    prepare_voxmovies(args.in_data, args.out_data, args.thread, args.downsampling, args.deleteZIP)
