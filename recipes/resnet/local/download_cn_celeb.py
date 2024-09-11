ffdc48ebdf08f147ffc04bead85429acb84f392d
Nicolas bataille <63408806+NicolasBataille@users.noreply.github.com>
2024-09-11 11:29:46 +0200

    Updated download method of cn_celeb to take into account the new check_md5 method

--- recipes/resnet/local/download_cn_celeb.py
+++ recipes/resnet/local/download_cn_celeb.py
@@ -14,7 +14,7 @@ import argparse
 
 
 CN_CELEB_PARTS_URL = [
-    ["https://www.openslr.org/resources/82/cn-celeb_v2.tar.gz", "7ab1b214028a7439e26608b2d5a0336c"],
+    # ["https://www.openslr.org/resources/82/cn-celeb_v2.tar.gz", "7ab1b214028a7439e26608b2d5a0336c"]
     ["https://www.openslr.org/resources/82/cn-celeb2_v2.tar.gzaa", "4cdf738cff565ce35ad34274848659c3"],
     ["https://www.openslr.org/resources/82/cn-celeb2_v2.tar.gzab", "4ff59a7009b79ef7043498ad3882b81b"],
     ["https://www.openslr.org/resources/82/cn-celeb2_v2.tar.gzac", "3aa8e6e7f7ec4382f9926b1b31498228"]
@@ -22,52 +22,66 @@ CN_CELEB_PARTS_URL = [
 
 
 
-def download_cn_celeb(target_dir: Pathlike = ".", force_download: Optional[bool] = False):
+def download_cn_celeb(target_dir: Pathlike = ".", force_download: Optional[bool] = False, max_retries: int = 3):
     target_dir = Path(target_dir)
     target_dir.mkdir(parents=True, exist_ok=True)
 
     cn_celeb2_tar_gz = "cn-celeb2.tar.gz"
     cn_celeb2_tar_gz_path = target_dir / cn_celeb2_tar_gz
+    
     cn_celeb1_tar_gz_path = target_dir / "cn-celeb_v2.tar.gz"
 
-    missing_files = [file.split("/")[-1] for file, _ in CN_CELEB_PARTS_URL if not (target_dir / file.split("/")[-1]).exists()]
-    if not missing_files and not force_download:
-        check_md5(target_dir, CN_CELEB_PARTS_URL)
-        if missing_files:
-            logging.info(f"Missing files: {', '.join(missing_files)}")
-        else:
-            logging.info("All files exist. Skipping download.")
-    else:
+    for retry in range(max_retries):
+        missing_files = [file.split("/")[-1] for file, _ in CN_CELEB_PARTS_URL if not (target_dir / file.split("/")[-1]).exists()]
+        invalid_files = []
+        
+        if not missing_files and not force_download:
+            all_files_valid = True
+            for url, md5 in CN_CELEB_PARTS_URL:
+                file_path = target_dir / url.split("/")[-1]
+                if not check_md5(file_path, md5):
+                    all_files_valid = False
+                    invalid_files.append(file_path)
+            
+            if all_files_valid:
+                logging.info("All files exist and have correct MD5 checksums. Skipping download.")
+                return
+            else:
+                logging.info(f"Some files have incorrect MD5 checksums. Removing and re-downloading (Attempt {retry + 1}/{max_retries}).")
+                for file in invalid_files:
+                    file.unlink()
+                missing_files.extend([f.name for f in invalid_files])
+        
         for url, md5 in CN_CELEB_PARTS_URL:
             fname = target_dir / url.split("/")[-1]
             if fname.name in missing_files or force_download:
                 urlretrieve_progress(url, filename=fname, desc=f"Downloading CN_Celeb {fname.name}")
+                if not check_md5(fname, md5):
+                    logging.error(f"MD5 check failed for {fname}. Retrying...")
+                    break
+        else:
+            break
+    else:
+        logging.error(f"Failed to download files with correct MD5 after {max_retries} attempts. Please try again later.")
+        return
 
-        check_md5(target_dir, CN_CELEB_PARTS_URL, "Cn_Celeb")
+    if any(f"cn-celeb2_v2.tar.gz{part}" in missing_files for part in ['aa', 'ab', 'ac']) or force_download:
+        logging.info("Building tar archive for CN_Celeb2...")
+        with open(cn_celeb2_tar_gz_path, "wb") as outFile:
+            for file in sorted(target_dir.glob("cn-celeb2_v2.tar.gza*")):
+                with open(file, "rb") as inFile:
+                    shutil.copyfileobj(inFile, outFile)
+        logging.info("Tar archive built, cleaning up...")
 
-        if any(f"cn-celeb2_v2.tar.gz{part}" in missing_files for part in ['aa', 'ab', 'ac']) or force_download:
-            logging.info("Building tar archive for CN_Celeb2...")
-            with open(cn_celeb2_tar_gz_path, "wb") as outFile:
-                for file in sorted(target_dir.glob("cn-celeb2_v2.tar.gza*")):
-                    with open(file, "rb") as inFile:
-                        shutil.copyfileobj(inFile, outFile)
-            logging.info("Tar archive built, cleaning up...")
+        for part in ["aa", "ab", "ac"]:
+            os.remove(target_dir / f"cn-celeb2_v2.tar.gz{part}")
 
-            for part in ["aa", "ab", "ac"]:
-                os.remove(target_dir / f"cn-celeb2_v2.tar.gz{part}")
+        logging.info(f"Unzipping CN_Celeb2 (train)...")
+        extract_tar(cn_celeb2_tar_gz_path, target_dir)
 
-            logging.info(f"Unzipping CN_Celeb2 (train)...")
-            extract_tar(cn_celeb2_tar_gz_path, target_dir)
-
-        if "cn-celeb_v2.tar.gz" in missing_files or force_download:
-            logging.info(f"Unzipping CN_Celeb1 (dev and test)...")
-            extract_tar(cn_celeb1_tar_gz_path, target_dir)
-            
-
-
-
-
-
+    if "cn-celeb_v2.tar.gz" in missing_files or force_download:
+        logging.info(f"Unzipping CN_Celeb1 (dev and test)...")
+        extract_tar(cn_celeb1_tar_gz_path, target_dir)
 
 
 if __name__ == '__main__':
