6d7634d6d28191f400a6888c9012bee35d249be2
mrouvier <mickael.rouvier@gmail.com>
2024-10-03 08:06:14 +0200

    new augmentation

--- kiwano/augmentation/base.py
+++ kiwano/augmentation/base.py
@@ -79,6 +79,28 @@ class Linear(Pipeline):
                 tensor, sample_rate = transform(tensor, sample_rate)
             return tensor, sample_rate
 
+
+
+class SpeedPerturbV3(Augmentation):
+    def __init__(self, factor: List[float]):
+        self.factor = factor
+
+    def __call__(self, tensor: torch.Tensor, sample_rate: int):
+        r = random.choice(self.factor)
+        wav, _ = torchaudio.sox_effects.apply_effects_tensor(tensor.unsqueeze(0), sample_rate, [['speed', str(r)], ["rate", str(sample_rate)]])
+        return wav[0], sample_rate
+
+
+
+class SpeedPerturb(Augmentation):
+    def __init__(self, factor: float):
+        self.factor = factor
+
+    def __call__(self, tensor: torch.Tensor, sample_rate: int):
+        wav, _ = torchaudio.sox_effects.apply_effects_tensor(tensor.unsqueeze(0), sample_rate, [['speed', str(self.factor)], ["rate", str(sample_rate)]])
+        return wav[0], sample_rate
+
+
 class SpeedPerturb(Augmentation):
     def __init__(self, factor: float):
         self.factor = factor
@@ -105,6 +127,42 @@ class Normal(Augmentation):
         return tensor, sample_rate
 
 
+
+
+class B12Attack(Augmentation):
+    def __init__(self, mu: float = 8e-2, sigma2: float = 2.5e-5, amplitude_threshold: float = 5e-2):
+        self.mu = mu
+        self.sigma2 = sigma2
+        self.amplitude_threshold = amplitude_threshold
+
+    def __call__(self, tensor: torch.Tensor, sample_rate: int):
+        total_samples = tensor.shape[0]
+
+        mu_samples = int(self.mu * sample_rate)
+        sigma_samples = int(np.sqrt(self.sigma2) * sample_rate)
+        segments = []
+        start = 0
+
+        while start < total_samples:
+            segment_duration = max(int(torch.normal(mu_samples, sigma_samples).item()), 1)
+            end = min(start + segment_duration, total_samples)
+            segment = tensor[start:end]
+    
+            if segment.abs().mean().item() >= self.amplitude_threshold:
+                segments.append(segment)
+
+            start = end
+
+        permuted_segments = segments.copy()
+        permuted_indices = torch.randperm(len(permuted_segments))
+        permuted_segments = [permuted_segments[i] for i in permuted_indices]
+
+        transformed_tensor = torch.cat(permuted_segments)
+        
+        return transformed_tensor, sample_rate
+
+
+
 class Noise(Augmentation):
     def __init__(self, segs: SegmentSet, snr_range: List[int]):
         self.segs = segs
@@ -113,7 +171,8 @@ class Noise(Augmentation):
 
     def __call__(self, tensor: torch.Tensor, sample_rate: int):
         snr_db = random.randint(self.snr_range[0], self.snr_range[1])
-        noise_tensor, noise_sample_rate = self.segs.get_random().load_audio()
+        rnd = self.segs.get_random()
+        noise_tensor, noise_sample_rate = rnd.load_audio()
 
         if len(noise_tensor) > len(tensor):
             start = random.randint(0, len(noise_tensor) - len(tensor))
@@ -125,15 +184,76 @@ class Noise(Augmentation):
             noise_tensor = noise_tensor[start:start + len(tensor)]
 
         speech_power = torch.linalg.vector_norm(tensor, ord=2) ** 2
-        noise_power = torch.linalg.vector_norm(noise_tensor, ord=2) ** 2
+        noise_power = torch.linalg.vector_norm(noise_tensor, ord=2) ** 2 + 1e-10
 
         original_snr_db = 10 * (torch.log10(speech_power) - torch.log10(noise_power))
         scale = 10 ** ((original_snr_db - snr_db) / 20.0)
         tensor = scale * noise_tensor + tensor
 
+        if torch.isnan(tensor).any():
+            print("ERRREUR")
+            print(rnd.segmentid)
+
+
         return tensor, sample_rate
 
 
+
+
+
+class Codec16kHzWithEffector(Augmentation):
+    def __init__(self, mp3_compression_range=(-9, 0), vorbis_compression_range=(-1, 10), opus_compression_range=(-1, 10), aac_compression_range=(64, 256), wma_compression_range=(48, 192)):
+        self.mp3_compression_range = mp3_compression_range
+        self.vorbis_compression_range = vorbis_compression_range
+        self.opus_compression_range = opus_compression_range
+        self.aac_compression_range = aac_compression_range
+        self.wma_compression_range = wma_compression_range
+                                                                    
+        self.codec = [
+            ({"format": "wav", "encoding": 'ULAW', "bits_per_sample": 8}, "8-bit mu-law"),
+            ({"format": "wav", "encoding": 'ALAW', "bits_per_sample": 8}, "8-bit a-law"),
+            ({"format": "mp3", "compression": None}, "MP3"),
+            ({"format": "vorbis", "compression": None}, "Vorbis"),
+            ({"format": "flac", "compression": 5}, "FLAC"),
+            #({"format": "opus", "compression": None}, "Opus"),
+            #({"format": "aac", "audio_bitrate": None}, "AAC"),
+            #({"format": "wma", "audio_bitrate": None}, "WMA")
+        ]
+
+    def __call__(self, tensor: torch.Tensor, sample_rate: int = 16000):
+        param, title = random.choice(self.codec)
+
+        if title == "MP3":
+            param["compression"] = random.uniform(*self.mp3_compression_range)
+        elif title == "Vorbis":
+            param["compression"] = random.uniform(*self.vorbis_compression_range)
+        elif title == "Opus":
+            param["compression"] = random.uniform(*self.opus_compression_range)
+        elif title == "AAC":
+            param["audio_bitrate"] = random.randint(*self.aac_compression_range) * 1000  # Convert to bps
+        elif title == "WMA":
+            param["audio_bitrate"] = random.randint(*self.wma_compression_range) * 1000  # Convert to bps
+
+
+        '''
+        effector = torchaudio.io.AudioEffector()
+        effector.set_output_format(param["format"])  # Set the output format (e.g., 'mp3', 'aac')
+        
+        if "bitrate" in param:
+            effector.set_codec_bitrate(param["bitrate"])
+        
+        if "compression" in param:
+            effector.set_codec_compression(param["compression"])
+        
+        if "bits_per_sample" in param:
+            ffector.set_bits_per_sample(param["bits_per_sample"])
+        
+        speech = effector.apply(tensor.unsqueeze(0), sample_rate)
+        '''
+
+        speech = torchaudio.functional.apply_codec(tensor.unsqueeze(0), sample_rate, **param)
+
+        return speech[0], sample_rate
 
 
 
@@ -142,8 +262,10 @@ class Codec(Augmentation):
         self.codec = [
             ({"format": "wav", "encoding": 'ULAW', "bits_per_sample": 8}, "8 bit mu-law"),
             ({"format": "wav", "encoding": 'ALAW', "bits_per_sample": 8}, "8 bit a-law"),
-            # ({"format": "mp3", "compression": -9}, "MP3"),
-            # ({"format": "vorbis", "compression": -1}, "Vorbis")
+            ({"format": "mp3", "compression": -9}, "MP3"),
+            ({"format": "vorbis", "compression": -1}, "Vorbis"),
+            ({"format": "mp3", "compression": -9}, "MP3"),
+            ({"format": "vorbis", "compression": -1}, "Vorbis")
         ]
 
         # {"format": "mp3", "codec_config": CodecConfig(compression_level=9)},
