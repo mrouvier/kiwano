36f0710762d4f014838cd25a7ee0a7b9e0f57fa3
mrouvier <mickael.rouvier@gmail.com>
2024-10-17 20:33:58 +0200

    add new model

--- kiwano/model/MHFA.py
+++ kiwano/model/MHFA.py
@@ -9,6 +9,137 @@ from .WavLM import *
 import torch
 import torch.nn as nn
 
+class ArcMarginProduct_intertopk_subcenter(nn.Module):
+    r"""Implement of large margin arc distance with intertopk and subcenter:
+        Reference:
+            MULTI-QUERY MULTI-HEAD ATTENTION POOLING AND INTER-TOPK PENALTY
+            FOR SPEAKER VERIFICATION.
+            https://arxiv.org/pdf/2110.05042.pdf
+            Sub-center ArcFace: Boosting Face Recognition by
+            Large-Scale Noisy Web Faces.
+            https://ibug.doc.ic.ac.uk/media/uploads/documents/eccv_1445.pdf
+        Args:
+            in_features: size of each input sample
+            out_features: size of each output sample
+            scale: norm of input feature
+            margin: margin
+            cos(theta + margin)
+            K: number of sub-centers
+            k_top: number of hard samples
+            mp: margin penalty of hard samples
+            do_lm: whether do large margin finetune
+        """
+
+    def __init__(self,
+                 in_features,
+                 out_features,
+                 scale=32.0,
+                 margin=0.2,
+                 easy_margin=False,
+                 K=3,
+                 mp=0.06,
+                 k_top=0,
+                 do_lm=False):
+        super(ArcMarginProduct_intertopk_subcenter, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.scale = scale
+        self.margin = margin
+        self.do_lm = do_lm
+
+        # intertopk + subcenter
+        self.K = K
+        if do_lm:  # if do LMF, remove hard sample penalty
+            self.mp = 0.0
+            self.k_top = 0
+        else:
+            self.mp = mp
+            self.k_top = k_top
+
+        # initial classifier
+        self.weight = nn.Parameter(
+            torch.FloatTensor(self.K * out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+
+        self.easy_margin = easy_margin
+        self.cos_m = math.cos(margin)
+        self.sin_m = math.sin(margin)
+        self.th = math.cos(math.pi - margin)
+        self.mm = math.sin(math.pi - margin) * margin
+        self.mmm = 1.0 + math.cos(
+            math.pi - margin)  # this can make the output more continuous
+        ########
+        self.m = self.margin
+        ########
+        self.cos_mp = math.cos(0.0)
+        self.sin_mp = math.sin(0.0)
+
+    def update(self, margin=0.2):
+        self.margin = margin
+        self.cos_m = math.cos(margin)
+        self.sin_m = math.sin(margin)
+        self.th = math.cos(math.pi - margin)
+        self.mm = math.sin(math.pi - margin) * margin
+        self.m = self.margin
+        self.mmm = 1.0 + math.cos(math.pi - margin)
+
+        # hard sample margin is increasing as margin
+        if margin > 0.001:
+            mp = self.mp * (margin / 0.2)
+        else:
+            mp = 0.0
+        self.cos_mp = math.cos(mp)
+        self.sin_mp = math.sin(mp)
+
+    def forward(self, input, label = None):
+        cosine = F.linear(F.normalize(input),
+                          F.normalize(self.weight))  # (batch, out_dim * k)
+        cosine = torch.reshape(
+            cosine, (-1, self.out_features, self.K))  # (batch, out_dim, k)
+        cosine, _ = torch.max(cosine, 2)  # (batch, out_dim)
+
+        if label == None:
+            return cosine
+
+        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        phi_mp = cosine * self.cos_mp + sine * self.sin_mp
+
+        if self.easy_margin:
+            phi = torch.where(cosine > 0, phi, cosine)
+        else:
+            ########
+            # phi = torch.where(cosine > self.th, phi, cosine - self.mm)
+            phi = torch.where(cosine > self.th, phi, cosine - self.mmm)
+            ########
+
+        one_hot = input.new_zeros(cosine.size())
+        one_hot.scatter_(1, label.view(-1, 1).long(), 1)
+
+        if self.k_top > 0:
+            # topk (j != y_i)
+            _, top_k_index = torch.topk(cosine - 2 * one_hot,
+                                        self.k_top)  # exclude j = y_i
+            top_k_one_hot = input.new_zeros(cosine.size()).scatter_(
+                1, top_k_index, 1)
+
+            # sum
+            output = (one_hot * phi) + (top_k_one_hot * phi_mp) + (
+                (1.0 - one_hot - top_k_one_hot) * cosine)
+        else:
+            output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
+        output *= self.scale
+        return output
+
+    def extra_repr(self):
+        return 'in_features={}, out_features={}, scale={}, margin={}, easy_margin={},' \
+            'K={}, mp={}, k_top={}, do_lm={}'.format(
+                self.in_features, self.out_features, self.scale, self.margin,
+                self.easy_margin, self.K, self.mp, self.k_top, self.do_lm)
+
+
+
+
 
 
 
@@ -37,6 +168,8 @@ class AMSMLoss(nn.Module):
     def reset_parameters(self):
         nn.init.xavier_uniform_(self.W)
 
+    def get_w(self):
+        return self.W
     
     def forward(self, input, label = None):
         # normalize features
@@ -131,9 +264,10 @@ class GradMultiply(torch.autograd.Function):
         return grad * ctx.scale, None
 
 
-class MHFA(nn.Module):
+
+class MHFA_subcenter(nn.Module):
     def __init__(self, name, number_head=32):
-        super(MHFA, self).__init__()
+        super(MHFA_subcenter, self).__init__()
         checkpoint = torch.load(name)
         cfg = WavLMConfig(checkpoint['cfg'])
         self.model = WavLM(cfg)
@@ -144,7 +278,8 @@ class MHFA(nn.Module):
 
         self.backend = MHFATop(head_nb=self.number_head)
 
-        self.output = AMSMLoss(256, 2, s=30, m=0.2)
+        #self.output = AMSMLoss(256, 2, s=30, m=0.2)
+        self.output = ArcMarginProduct_intertopk_subcenter(256, 2)
 
 
     def forward(self, wav_and_flag, iden = None):
@@ -172,6 +307,193 @@ class MHFA(nn.Module):
 
         out = self.output(out, iden)
         return out
+
+    def get_m(self):
+        return 0
+
+
+    def loadParameters(self, param):
+
+        self_state = self.model.state_dict();
+        loaded_state = param
+
+        for name, param in loaded_state.items():
+            origname = name;
+            
+
+            if name not in self_state:
+                # print("%s is not in the model."%origname);
+                continue;
+
+            if self_state[name].size() != loaded_state[origname].size():
+                print("Wrong parameter length: %s, model: %s, loaded: %s"%(origname, self_state[name].size(), loaded_state[origname].size()));
+                continue;
+
+            self_state[name].copy_(param);
+
+
+
+class MHFALarge(nn.Module):
+    def __init__(self, name, number_head=32):
+        super(MHFALarge, self).__init__()
+        checkpoint = torch.load(name)
+        cfg = WavLMConfig(checkpoint['cfg'])
+        self.model = WavLM(cfg)
+        self.loadParameters(checkpoint['model'])
+        self.number_head = number_head
+
+        #self.model = AutoModel.from_pretrained(name)
+
+        self.backend = MHFATop(head_nb=self.number_head, inputs_dim=1024, number_layers=14)
+
+        self.output = AMSMLoss(256, 2, s=30, m=0.2)
+
+
+    def get_w(self):
+        return self.output.get_w()
+
+    def forward(self, wav_and_flag, iden = None):
+        '''
+        out = self.model(wav_and_flag, output_hidden_states=True)
+
+        hidden_states = out.hidden_states
+
+        layer_reps = [x for x in hidden_states]
+
+        x = torch.stack(layer_reps)
+        x = x.permute(1, 3, 2, 0)
+        '''
+
+        x = wav_and_flag
+        cnn_outs, layer_results =  self.model.extract_features(x, output_layer=13)
+        layer_reps = [x.transpose(0, 1) for x, _ in layer_results]
+        x = torch.stack(layer_reps).transpose(0,-1).transpose(0,1)
+
+        out = self.backend(x)
+
+
+        if iden == None:
+            return self.output(out)
+
+        out = self.output(out, iden)
+        return out
+
+    def emb(self, wav_and_flag, iden = None):
+        '''
+        out = self.model(wav_and_flag, output_hidden_states=True)
+
+        hidden_states = out.hidden_states
+
+        layer_reps = [x for x in hidden_states]
+
+        x = torch.stack(layer_reps)
+        x = x.permute(1, 3, 2, 0)
+        '''
+
+        x = wav_and_flag
+        cnn_outs, layer_results =  self.model.extract_features(x, output_layer=13)
+        layer_reps = [x.transpose(0, 1) for x, _ in layer_results]
+        x = torch.stack(layer_reps).transpose(0,-1).transpose(0,1)
+
+        out = self.backend(x)
+
+        return out
+
+
+    def get_m(self):
+        return 0
+
+
+    def loadParameters(self, param):
+
+        self_state = self.model.state_dict();
+        loaded_state = param
+
+        for name, param in loaded_state.items():
+            origname = name;
+            
+
+            if name not in self_state:
+                # print("%s is not in the model."%origname);
+                continue;
+
+            if self_state[name].size() != loaded_state[origname].size():
+                print("Wrong parameter length: %s, model: %s, loaded: %s"%(origname, self_state[name].size(), loaded_state[origname].size()));
+                continue;
+
+            self_state[name].copy_(param);
+
+
+
+
+
+
+class MHFA(nn.Module):
+    def __init__(self, name, number_head=32):
+        super(MHFA, self).__init__()
+        checkpoint = torch.load(name)
+        cfg = WavLMConfig(checkpoint['cfg'])
+        self.model = WavLM(cfg)
+        self.loadParameters(checkpoint['model'])
+        self.number_head = number_head
+
+        #self.model = AutoModel.from_pretrained(name)
+
+        self.backend = MHFATop(head_nb=self.number_head)
+
+        self.output = AMSMLoss(256, 2, s=30, m=0.2)
+
+
+    def get_w(self):
+        return self.output.get_w()
+
+    def forward(self, wav_and_flag, iden = None):
+        '''
+        out = self.model(wav_and_flag, output_hidden_states=True)
+
+        hidden_states = out.hidden_states
+
+        layer_reps = [x for x in hidden_states]
+
+        x = torch.stack(layer_reps)
+        x = x.permute(1, 3, 2, 0)
+        '''
+
+        x = wav_and_flag
+        cnn_outs, layer_results =  self.model.extract_features(x, output_layer=13)
+        layer_reps = [x.transpose(0, 1) for x, _ in layer_results]
+        x = torch.stack(layer_reps).transpose(0,-1).transpose(0,1)
+
+        out = self.backend(x)
+
+
+        if iden == None:
+            return self.output(out)
+
+        out = self.output(out, iden)
+        return out
+
+    def emb(self, wav_and_flag, iden = None):
+        '''
+        out = self.model(wav_and_flag, output_hidden_states=True)
+
+        hidden_states = out.hidden_states
+
+        layer_reps = [x for x in hidden_states]
+
+        x = torch.stack(layer_reps)
+        x = x.permute(1, 3, 2, 0)
+        '''
+
+        x = wav_and_flag
+        cnn_outs, layer_results =  self.model.extract_features(x, output_layer=13)
+        layer_reps = [x.transpose(0, 1) for x, _ in layer_results]
+        x = torch.stack(layer_reps).transpose(0,-1).transpose(0,1)
+
+        out = self.backend(x)
+
+        return out
+
 
     def get_m(self):
         return 0
--- kiwano/model/__init__.py
+++ kiwano/model/__init__.py
@@ -3,4 +3,4 @@ from .ecapa_tdnn_model import *
 from .schedulers import *
 from .loss import *
 from .automodel import *
-#from .MHFA import *
+from .MHFA import *
