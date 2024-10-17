edcd423c423cf9d8f8717d137792148219d3ef18
mrouvier <mickael.rouvier@gmail.com>
2024-10-17 20:42:10 +0200

    extract and scoring tools

--- recipes/asvspoof/extract_xvector.sh
+++ recipes/asvspoof/extract_xvector.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=Kiwano
 #SBATCH --cpus-per-task=10
-#SBATCH --time=0:20:00
+#SBATCH --time=0:50:00
 #SBATCH -A rmw@v100
 #SBATCH --qos=qos_gpu-t3
 #SBATCH --array=0-20
@@ -20,5 +20,25 @@ module load pytorch-gpu/py3/2.2.0
 dir=$1
 
 mkdir -p ${dir}/asvspoof.${2}/
+mkdir -p ${dir}/cfad_noisy.${2}/
+mkdir -p ${dir}/cfad_codec.${2}/
+mkdir -p ${dir}/latin_american.${2}/
+mkdir -p ${dir}/itw.${2}/
+#mkdir -p ${dir}/defi.${2}/
+#mkdir -p ${dir}/defi_taskb.${2}/
 
-python3 utils/extract_resnet.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/ASVSpoof5_dev/ ${dir}/model${2}.ckpt pkl:${dir}/asvspoof.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+
+python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/latin_american/ ${dir}/model${2}.ckpt pkl:${dir}/latin_american.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/ASVSpoof5_dev/ ${dir}/model${2}.ckpt pkl:${dir}/asvspoof.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/cfad_noisy_version/ ${dir}/model${2}.ckpt pkl:${dir}/cfad_noisy.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/cfad_codec_version/ ${dir}/model${2}.ckpt pkl:${dir}/cfad_codec.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/itw/ ${dir}/model${2}.ckpt pkl:${dir}/itw.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+#python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/defi/ ${dir}/model${2}.ckpt pkl:${dir}/defi.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
+
+#python3 utils/extract_mhfa.py --world_size=$SLURM_ARRAY_TASK_COUNT  --rank=$SLURM_ARRAY_TASK_ID data/defi_taskb/ ${dir}/model${2}.ckpt pkl:${dir}/defi_taskb.${2}/xvector.$SLURM_ARRAY_TASK_ID.pkl
